c43c934712dec5e6c987ca43c53d06257e4c3df0
annamile92 <anamibz.dv@gmail.com>
2025-11-23 22:14:16 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -77,11 +77,7 @@ export default function Home() {
 
       <section id="evento" className="py-32 px-6 text-center">
         <h2 className="text-4xl font-bold mb-6">Evento</h2>
-        <p className="opacity-80">En Medellín, el running ha crecido notablemente en los últimos años, pasando de ser una práctica individual de salud a convertirse en un fenómeno social. Cada vez más personas participan en grupos de corredores, carreras locales y entrenamientos colectivos. 
-
-Más allá de los beneficios físicos, los corredores buscan compartir experiencias, motivarse mutuamente y generar vínculos sociales. 
-
-El running ofrece una forma de encuentro natural, distinta a las dinámicas directas de las apps de citas o redes sociales, donde el ejercicio se convierte en un puente hacia la amistad y la comunidad.</p>
+        <p className="opacity-80">Justificacion</p>
       </section>
 
       <section id="app" className="py-32 px-6 text-center">
