50021afbc0a2015b8026e4e8df78fcaf81e430d9
annamile92 <anamibz.dv@gmail.com>
2026-02-16 16:44:25 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -127,30 +127,7 @@ bg-gradient-to-r from-gray-800/80 to-gray-500/60 backdrop-blur-lg z-50">
         </div>
       </section>
 {/* DOCUMENTOS */}
-      <section id="documentos" className="py-16 px-6 flex flex-col md:flex-row items-center gap-6 bg-[#2e2e2ecc]">
-        <div className="md:w-1/2 flex justify-center">
-          <Image src="/3.png" alt="Documentos" width={280} height={280} className="rounded-lg shadow-md" />
-        </div>
-        <div className="md:w-1/2 text-white flex flex-col gap-4">
-          <h2 className="text-3xl font-bold mb-4">Documentos</h2>
-          <p className="opacity-80">Documentos investigación del proyecto Run4Fun.</p>
-
-          <div className="flex gap-4 mt-4">
-
-            <a href="https://www.figma.com/deck/O6dXDo8EnfQbRB2ELgYmOw"  target="_blank"
-  rel="noopener noreferrer">
-  <Image src="/archivo.png" alt="Archivo 2" width={60} height={60} />
-</a>
-<a
-  href="/docs/Proyecto Run4Fun - Investigacion.docx"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <Image src="/archivo.png" alt="Archivo 2" width={60} height={60} />
-</a>
-            
-          </div>
-        </div>
+      <section>
       </section>
 {/* FOOTER */}
       <footer className="py-8 px-5 text-center bg-[#3a3a3acc]">
